daefa3ab3a8fb83838ef53c1f5e9eba11f2b02f4
Ivan <sidorenko.evan@yandex.ru>
2022-09-22 21:22:43 +0300

    WebTests done

---
+++ src/test/java/java/axamit/config/Credentials.java
@@ -0,0 +1,15 @@
+package java.axamit.config;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class Credentials {
+    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class, System.getProperties());
+
+    public static boolean isRemoteWebDriver() {
+        return !config.remote().equals("");
+    }
+
+    public static boolean isVideoOn() {
+        return !config.videoStorage().equals("");
+    }
+}
---
+++ src/test/java/java/axamit/pages/JobPage.java
@@ -0,0 +1,20 @@
+package java.axamit.pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class JobPage {
+    private final SelenideElement careerTab = $("a[data-menuactivation=\"career\"]");
+    private final SelenideElement accordion = $("a[data-part=\"junior-javaaem-developer\"]");
+
+    public void clickOnCareerTab() {
+        careerTab.click();
+    }
+
+    public void checkTheJobInAccordion(String accordionData) {
+        accordion.shouldHave(Condition.text(accordionData));
+    }
+
+}
